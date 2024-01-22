fe2c3f68061af37f191e2231293afacf390cabda
Anshor J <lordlexa10@gmail.com>
2024-01-22 10:01:54 +0700

    Change wording and remove some properties

--- resources/views/components/donor-info.blade.php
+++ resources/views/components/donor-info.blade.php
@@ -1,7 +1,7 @@
 <div class="text-sm py-4 grid grid-cols-1">
     <table>
         <tr class="flex">
-            <td class="w-32 pl-2 py-1 items-start">ID Muzaki</td>
+            <td class="w-32 pl-2 py-1 items-start">ID Donatur</td>
             <td class="w-full py-1 whitespace-normal font-semibold">
                 {{ $donor->getAttribute('donor_identification_number') }}
             </td>
@@ -10,24 +10,6 @@
             <td class="w-32 pl-2 py-1 items-start">Nama</td>
             <td class="w-full py-1 whitespace-normal font-semibold">
                 {{ $donor->getAttribute('donor_name') }}
-            </td>
-        </tr>
-        <tr class="flex">
-            <td class="w-32 pl-2 py-1 items-start">Alamat</td>
-            <td class="w-full py-1 whitespace-normal font-semibold">
-                {{ $donor->getAttribute('donor_address') ?? '-' }}
-            </td>
-        </tr>
-        <tr class="flex">
-            <td class="w-32 pl-2 py-1 items-start">No. Telp</td>
-            <td class="w-full py-1 whitespace-normal font-semibold">
-                {{ $donor->getAttribute('donor_phone_number') ?? '-' }}
-            </td>
-        </tr>
-        <tr class="flex">
-            <td class="w-32 pl-2 py-1 items-start">NPWP</td>
-            <td class="w-full py-1 whitespace-normal font-semibold">
-                {{ $donor->getAttribute('donor_tax_number') ?? '-' }}
             </td>
         </tr>
         <tr class="flex">
