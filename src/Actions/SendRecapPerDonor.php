b4130bdb78d81bb3c87acf31091cf2e303594fc0
Anshor J <lordlexa10@gmail.com>
2024-02-06 10:07:41 +0700

    Fix : error when sending donor

--- src/Actions/SendRecapPerDonor.php
+++ src/Actions/SendRecapPerDonor.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Inisiatif\DonationRecap\Actions;
 
 use Inisiatif\DonationRecap\Models\DonationRecap;
-use Inisiatif\DonationRecap\Enums\DonationRecapState;
+use Inisiatif\DonationRecap\Enums\ProcessingState;
 use Inisiatif\DonationRecap\Jobs\SendingRecapPerDonor;
 use Inisiatif\DonationRecap\Models\DonationRecapDonor;
 use Inisiatif\DonationRecap\Exceptions\CannotSendRecap;
@@ -19,14 +19,14 @@ final class SendRecapPerDonor
         /** @var DonationRecap $recap */
         $recap = $donor->getRelation('recap');
 
-        if ($recap->inState(DonationRecapState::combined) === false) {
+        if ($donor->inState(ProcessingState::combined) === false) {
             throw CannotSendRecap::make('must be in `combined` state');
         }
 
         \dispatch(new SendingRecapPerDonor($recap, $donor));
 
         $recap->recordHistory(
-            'Memproses pengiriman rekap donasi a/n '.$donor->getAttribute('donor_name'),
+            'Memproses pengiriman rekap donasi a/n ' . $donor->getAttribute('donor_name'),
             $donor->getAttribute('donor_id')
         );
     }
