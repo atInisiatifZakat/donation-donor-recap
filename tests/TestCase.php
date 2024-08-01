31351dddcc2f0723fbf9408d27256f67ccbbe8b5
Anshor J <lordlexa10@gmail.com>
2024-08-01 16:42:24 +0700

    Add some default config and migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Inisiatif\DonationRecap\Tests;
 
+use function Orchestra\Testbench\artisan;
+
+use Illuminate\Support\Facades\DB;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Inisiatif\DonationRecap\DonationRecapServiceProvider;
+
 
 abstract class TestCase extends Orchestra
 {
@@ -19,5 +23,33 @@ abstract class TestCase extends Orchestra
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
+        config()->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        config()->set('database.default', 'testing');
+        config()->set('recap.recording.donor_table_name', 'donors');
+        config()->set('recap.recording.employee_table_name', 'employees');
+    }
+
+    protected function defineDatabaseMigrations()
+    {
+
+        $appMigrationPath = __DIR__ . '/../database/migrations';
+        $testMigrationPath = __DIR__ . '/../tests/Migrations';
+
+        $this->loadMigrationsFrom($appMigrationPath);
+
+        artisan($this, 'migrate', ['--database' => 'testing']);
+
+        $this->loadMigrationsFrom($testMigrationPath);
+
+        artisan($this, 'migrate', ['--database' => 'testing']);
+
+        $this->beforeApplicationDestroyed(
+            fn () => artisan($this, 'migrate:rollback', ['--database' => 'testing'])
+        );
     }
 }
