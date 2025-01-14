f32601ba224c20974531b15de02a5f6790feb63b
nuradiyana <1073802+nuradiyana@users.noreply.github.com>
2025-01-14 09:39:46 +0000

    Fix styling

--- src/Builders/DonationRecapDetailBuilder.php
+++ src/Builders/DonationRecapDetailBuilder.php
@@ -24,38 +24,38 @@ final class DonationRecapDetailBuilder
             ->select($this->getSelectStatement())
             ->join(
                 self::getDonationTable(),
-                self::getDonationTable() . '.id',
+                self::getDonationTable().'.id',
                 '=',
-                self::getDonationDetailTable() . '.donation_id'
+                self::getDonationDetailTable().'.donation_id'
             )
             ->join(
                 self::getFundingTypeTable(),
-                self::getFundingTypeTable() . '.id',
+                self::getFundingTypeTable().'.id',
                 '=',
-                self::getDonationDetailTable() . '.funding_type_id'
+                self::getDonationDetailTable().'.funding_type_id'
             )
             ->join(
                 self::getFundingCategoryTable(),
-                self::getFundingCategoryTable() . '.id',
+                self::getFundingCategoryTable().'.id',
                 '=',
-                self::getFundingTypeTable() . '.funding_category_id'
+                self::getFundingTypeTable().'.funding_category_id'
             )
             ->leftJoin(
                 self::getFundingGoodTable(),
-                self::getFundingGoodTable() . '.id',
+                self::getFundingGoodTable().'.id',
                 '=',
-                self::getDonationDetailTable() . '.funding_good_id'
+                self::getDonationDetailTable().'.funding_good_id'
             )
             ->leftJoin(
                 self::getProgramTable(),
-                self::getProgramTable() . '.id',
+                self::getProgramTable().'.id',
                 '=',
-                self::getDonationDetailTable() . '.program_id'
+                self::getDonationDetailTable().'.program_id'
             )
-            ->where(self::getDonationTable() . '.donor_id', $donor->getAttribute('donor_id'))
-            ->where(self::getDonationTable() . '.transaction_status', 'VERIFIED')
-            ->whereNull(self::getDonationDetailTable() . '.deleted_at')
-            ->whereBetween(self::getDonationTable() . '.transaction_date', [
+            ->where(self::getDonationTable().'.donor_id', $donor->getAttribute('donor_id'))
+            ->where(self::getDonationTable().'.transaction_status', 'VERIFIED')
+            ->whereNull(self::getDonationDetailTable().'.deleted_at')
+            ->whereBetween(self::getDonationTable().'.transaction_date', [
                 $recap->getPeriodStartDate()->startOfDay(),
                 $recap->getPeriodEndDate()->endOfDay(),
             ])->chunkById(1000, function (Collection $items) use ($recap): void {
@@ -70,31 +70,32 @@ final class DonationRecapDetailBuilder
                     ];
                     unset($data['donation_detail_id']);
                     unset($data['donation_funding_type_public_name']);
+
                     return $data;
                 })->toArray();
 
                 DB::table('donation_recap_details')->insert($attributes);
-            }, self::getDonationDetailTable() . '.id', 'donation_detail_id');
+            }, self::getDonationDetailTable().'.id', 'donation_detail_id');
     }
 
     private function getSelectStatement(): array
     {
         return [
-            DB::raw(self::getDonationDetailTable() . '.id as donation_detail_id'),
-            DB::raw(self::getDonationTable() . '.transaction_date as donation_transaction_date'),
-            DB::raw(self::getDonationTable() . '.identification_number as donation_identification_number'),
-            DB::raw(self::getDonationTable() . '.type as donation_type'),
-            DB::raw(self::getFundingCategoryTable() . '.id as donation_funding_category_id'),
-            DB::raw(self::getFundingCategoryTable() . '.name as donation_funding_category_name'),
-            DB::raw(self::getFundingTypeTable() . '.name as donation_funding_type_name'),
-            DB::raw(self::getFundingTypeTable() . '.public_name as donation_funding_type_public_name'),
-            DB::raw(self::getProgramTable() . '.name as donation_program_name'),
-            DB::raw(self::getDonationDetailTable() . '.good_name as donation_good_name'),
-            DB::raw(self::getDonationDetailTable() . '.good_quantity as donation_good_quantity'),
-            DB::raw(self::getFundingGoodTable() . '.unit as donation_good_unit'),
-            DB::raw(self::getDonationDetailTable() . '.total_amount as donation_amount'),
-            self::getDonationDetailTable() . '.donation_id',
-            self::getDonationTable() . '.donor_id',
+            DB::raw(self::getDonationDetailTable().'.id as donation_detail_id'),
+            DB::raw(self::getDonationTable().'.transaction_date as donation_transaction_date'),
+            DB::raw(self::getDonationTable().'.identification_number as donation_identification_number'),
+            DB::raw(self::getDonationTable().'.type as donation_type'),
+            DB::raw(self::getFundingCategoryTable().'.id as donation_funding_category_id'),
+            DB::raw(self::getFundingCategoryTable().'.name as donation_funding_category_name'),
+            DB::raw(self::getFundingTypeTable().'.name as donation_funding_type_name'),
+            DB::raw(self::getFundingTypeTable().'.public_name as donation_funding_type_public_name'),
+            DB::raw(self::getProgramTable().'.name as donation_program_name'),
+            DB::raw(self::getDonationDetailTable().'.good_name as donation_good_name'),
+            DB::raw(self::getDonationDetailTable().'.good_quantity as donation_good_quantity'),
+            DB::raw(self::getFundingGoodTable().'.unit as donation_good_unit'),
+            DB::raw(self::getDonationDetailTable().'.total_amount as donation_amount'),
+            self::getDonationDetailTable().'.donation_id',
+            self::getDonationTable().'.donor_id',
         ];
     }
 }
