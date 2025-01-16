c965cac61aa0e868a1fc497375186f025d3b41fc
Anshor J <lordlexa10@gmail.com>
2025-01-16 09:56:33 +0700

    Fixing minor

--- src/Builders/DonationRecapDetailBuilder.php
+++ src/Builders/DonationRecapDetailBuilder.php
@@ -7,6 +7,7 @@ namespace Inisiatif\DonationRecap\Builders;
 use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Inisiatif\DonationRecap\Enums\DonationRecapState;
 use Inisiatif\DonationRecap\Traits\HasTableName;
 use Inisiatif\DonationRecap\Models\DonationRecap;
 use Inisiatif\DonationRecap\Models\DonationRecapDonor;
@@ -58,7 +59,7 @@ final class DonationRecapDetailBuilder
             ->whereBetween(self::getDonationTable().'.transaction_date', [
                 $recap->getPeriodStartDate()->startOfDay(),
                 $recap->getPeriodEndDate()->endOfDay(),
-            ])->chunkById(1000, function (Collection $items) use ($recap): void {
+            ])->chunkById(1000, function (Collection $items) use ($recap, $donor): void {
                 $attributes = $items->map(static function (object $attribute) use ($recap): array {
                     $data = [
                         ...(array) $attribute,
