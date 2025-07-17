3b220dae741cd36123aefb1e80bd5f7dd68c5fc8
ihsanfikri12 <fikriabiyyu@gmail.com>
2025-07-17 15:13:16 +0700

    change rate limit

--- src/Jobs/SendingRecapPerDonor.php
+++ src/Jobs/SendingRecapPerDonor.php
@@ -26,9 +26,9 @@ final class SendingRecapPerDonor implements ShouldQueue
     use Queueable;
     use SerializesModels;
 
-    private const MAX_ATTEMPTS = 75;
+    private const MAX_ATTEMPTS = 40;
 
-    private const DELAY_SECONDS = 1;
+    private const DELAY_SECONDS = 3;
 
     private const RATE_LIMITER_KEY = 'sending-recap';
 
